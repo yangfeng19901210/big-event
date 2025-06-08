bd891e61738b43eca40fe799e3120d6ccd7a6997
yangFeng <15525438831@163.com>
2025-06-08 16:22:05 +0800

    认证异常统一处理

--- src/main/java/com/yy/config/GlobalExceptionHandler.java
+++ src/main/java/com/yy/config/GlobalExceptionHandler.java
@@ -14,27 +14,48 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class GlobalExceptionHandler {
     /**
-     * 处理断言异常，并返回数据
+     * 处理常见异常
      * @param
      * @return
      * @throws
      * @author :loulan
      */
     @ExceptionHandler({IllegalArgumentException.class, AssertException.class, BusinessException.class, BaseException.class})
-    public Result handlerAssertException(Exception ex) {
+    public Result handlerCommonException(Exception ex) {
         log.error("发生业务异常", ex);
         return Result.error(ex.getMessage());
     }
+    /**
+    * @description 处理参数校验异常
+    * @author yangFeng
+    * @date 2025/6/8 16:20
+    * @param ex
+    * @return com.yy.common.response.Result
+    */
     @ExceptionHandler(ConstraintViolationException.class)
-    public Result handlerException(ConstraintViolationException ex) {
+    public Result handlerValidationException(ConstraintViolationException ex) {
         log.error("发生异常", ex);
         return Result.error(ex.getMessage());
     }
+    /**
+    * @description 处理token认证异常
+    * @author yangFeng
+    * @date 2025/6/8 16:20
+    * @param ex
+    * @return com.yy.common.response.Result
+    */
     @ExceptionHandler(AuthException.class)
-    public Result handlerException(AuthException ex) {
+    public Result handlerAuthException(AuthException ex) {
         log.error("发生异常", ex);
         return Result.error(401,ex.getMessage());
     }
+    /**
+    * @description 处理未知异常（最终的兜底处理）
+    * @author yangFeng
+    * @date 2025/6/8 16:21
+    * @param e
+    * @return com.yy.common.response.Result
+    */
     @ExceptionHandler(Exception.class)
     public Result handleException(Exception e){
         e.printStackTrace();
