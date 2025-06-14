8c04bddfe4dd34b47d6b3d03b2d440d7b9c4d0c6
yangFeng <15525438831@163.com>
2025-06-14 12:35:38 +0800

    实体类整合

--- src/main/java/com/yy/pojo/Article.java
+++ src/main/java/com/yy/pojo/Article.java
@@ -1,11 +1,8 @@
 package com.yy.pojo;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.util.Date;
-
 import com.yy.common.entity.BaseEntity;
 import lombok.Data;
 
