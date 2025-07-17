467e91932b6225e374b91f7f1710efcc71d0fab4
15525438831 <15525438831@163.com>
2025-07-17 11:41:53 +0800

    自定义校验规则进行校验

--- src/main/java/com/yy/anno/ArticleState.java
+++ src/main/java/com/yy/anno/ArticleState.java
@@ -17,7 +17,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Constraint(validatedBy = {ArticleStateValidation.class}) //指定校验器
 public @interface ArticleState {
     //提供校验失败后的提示信息
-    String message() default "{state参数的值只能是已发布或者草稿}";
+    String message() default "state参数的值只能是已发布或者草稿";
     //指定分组
     Class<?>[] groups() default { };
     // 负载,获取到ArticleState注解的附加信息
--- src/main/java/com/yy/config/WebConfig.java
+++ src/main/java/com/yy/config/WebConfig.java
@@ -1,7 +1,7 @@
 package com.yy.config;
 
 import com.yy.interceptors.LoginInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Autowired
+    @Resource
     private LoginInterceptor loginInterceptor;
 
     @Override
