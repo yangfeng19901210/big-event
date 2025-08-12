28d85fb270c20019180dfab5c5dd07a136695560
15525438831 <15525438831@163.com>
2025-08-12 09:07:18 +0800

    删除不使用的类

--- src/main/java/com/yy/controller/RoleController.java
+++ src/main/java/com/yy/controller/RoleController.java
@@ -1,7 +1,6 @@
 package com.yy.controller;
 
 import com.yy.common.entity.PageDTO;
-import com.yy.pojo.CustomUser;
 import com.yy.pojo.SysRole;
 import com.yy.service.SysRoleService;
 import com.yy.vo.out.RolePageOutVO;
@@ -9,8 +8,6 @@ import com.yy.vo.query.RolePageQueryVO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
